15c214f802da1696dfb50b71a7167dde92850a4e
hareru2010 <ciao.shime2010m@gmail.com>
2024-12-27 17:46:26 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+console.log("test")
+
+document.addEventListener("DOMContentLoaded", function() {
+    // ページ内のリンクをすべて取得
+    const links = document.querySelectorAll('a[href^="#"]');
+  
+    links.forEach(link => {
+      link.addEventListener("click", function(event) {
+        // デフォルトのリンク動作を無効にする
+        event.preventDefault();
+  
+        // リンク先のhref属性を取得
+        let href = this.getAttribute("href");
+  
+        // ジャンプ先の要素を取得
+        let target = document.querySelector(href === "#" || href === "" ? "html" : href);
+  
+        if (target) {
+          // ジャンプ先の要素の位置を取得
+          let position = target.getBoundingClientRect().top + window.scrollY;
+  
+          // スムーススクロールを実行
+          window.scrollTo({
+            top: position,
+            behavior: "smooth" // スムーススクロールを指定
+          });
+        }
+      });
+    });
+  });
